8dc8b899f09f66da10b87b79a720b8e72be3dcb8
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-03-24 06:15:14 +0000

    Update cats-effect to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scala3 = "3.3.5"
     val borer = "1.16.0"
     val catsCore = "2.13.0"
-    val catsEffect = "3.5.7"
+    val catsEffect = "3.6.0"
     val http4s = "0.23.30"
     val munit = "1.1.0"
     val munitCatsEffect = "2.0.0"
